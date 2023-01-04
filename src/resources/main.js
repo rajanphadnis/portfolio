4520199a5f650ec5c971e3ba80c1cb692e6b3bc2
Rajan Phadnis <rajansd28@gmail.com>
2023-01-04 00:21:08 -0800

    terrible skeumorphic buttons

--- src/resources/main.js
+++ src/resources/main.js
@@ -1,8 +1,9 @@
-function openCity(projectType) {
+function openCity(projectType, buttonTrig) {
     var i;
     var x = document.getElementsByClassName("proj");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";  
     }
+    document.getElementById(buttonTrig).active = true;
     document.getElementById(projectType).style.display = "block";  
   }
