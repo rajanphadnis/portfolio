397a1542615110f7295b0f6bdef7ef1903c80558
Rajan Phadnis <rajansd28@gmail.com>
2023-01-05 18:18:01 -0800

    added basic cards

--- src/resources/main.js
+++ src/resources/main.js
@@ -1,9 +1,8 @@
-function openCity(projectType, buttonTrig) {
+function openCity(projectType) {
     var i;
     var x = document.getElementsByClassName("proj");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";  
     }
-    document.getElementById(buttonTrig).active = true;
     document.getElementById(projectType).style.display = "block";  
   }
