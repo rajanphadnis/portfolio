0c531c4ef2798376dc4cc7ee375b3b6a85cc8fe7
Rajan Phadnis <22627030+rajanphadnis@users.noreply.github.com>
2023-04-18 15:24:13 -0400

    added compressible calculator

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   // Copy `src/404.html` to `public/404.html`
   eleventyConfig.addPassthroughCopy("src/404.html");
+  eleventyConfig.addPassthroughCopy("src/compressible-calculator/index.html");
   // Copy `src/resources/` to `public/resources/`
   eleventyConfig.addPassthroughCopy("src/resources");
     return {
